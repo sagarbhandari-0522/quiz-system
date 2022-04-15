3f1d7628c4ed4daf1fc7568298369c601ed879bd
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-04-15 11:15:01 +0545

    fix : reload on submitting data to form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'dashboards/index'
   devise_for :users
+  resources :dashboards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
 end
