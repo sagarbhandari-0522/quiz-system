a83ad88b76c5f528fa478ca8ebb6fd742cab04b4
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-04-17 09:30:49 +0545

    feat:maintaining association between question option and category

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  has_many :question_categories
+  has_many :categories, through: :question_categories
+  has_many :options
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_17_031636) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_17_032313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_031636) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "question_id", null: false
+    t.index ["question_id"], name: "index_options_on_question_id"
   end
 
   create_table "question_categories", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_031636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "options", "questions"
 end
