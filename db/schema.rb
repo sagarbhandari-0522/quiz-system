2b575c1955bdd999cda3f865144f900342f2f4c1
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-04-17 08:08:35 +0545

    feat:create question_category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_16_145912) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_17_021907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_16_145912) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "question_categories", force: :cascade do |t|
+    t.bigint "question_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_question_categories_on_category_id"
+    t.index ["question_id"], name: "index_question_categories_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
