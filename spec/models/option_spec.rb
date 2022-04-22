ff937ddeaa0edb201c195ce92d6b176d8d67a268
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-04-22 12:40:45 +0545

    fix:rspec for option model

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -9,16 +9,22 @@ RSpec.describe Option, type: :model do
       expect(subject).to respond_to(:title)
     end
   end
-  describe 'presence of attributes value' do
-    it 'title should  value' do
+  describe 'association' do
+    it { should belong_to(:question) }
+  end
+
+  describe 'validation' do
+    it 'should have title' do
       expect(subject).to be_valid
+    end
+    it 'should not accept without title' do
       subject.title = nil
       expect(subject).to_not be_valid
     end
-  end
-  describe 'association' do
     it 'should associated with question' do
       expect(subject).to be_valid
+    end
+    it 'should not accept without question' do
       subject.question = nil
       expect(subject).to_not be_valid
     end
