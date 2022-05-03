410990b96397d8fd7d8b68d507438bdd1177e372
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-05-03 20:12:27 +0545

    feat:add quiz policy for authorization

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,7 +2,11 @@ class QuizzesController < ApplicationController
   before_action :authenticate_user!
   before_action :find_quiz, only: %i[show destroy]
   def index
-    @quizzes = Quiz.all
+    @quizzes = if current_user.admin?
+                 Quiz.all
+               else
+                 Quiz.where(user_id: current_user.id)
+               end
   rescue StandardError => e
     render body: e.message
   end
@@ -11,7 +15,8 @@ class QuizzesController < ApplicationController
 
   def new
     @quiz = Quiz.new
-    @questions = QuestionCategory.where(category_id: params[:category_ids].drop(1)).sample(5)
+    authorize @quiz
+    @questions = QuestionCategory.where(category_id: params[:category_ids].drop(1)).uniq.sample(5)
     if @questions.count < 5
       flash[:danger] = "Your selected category dont have enough question \n Please Reselect "
       redirect_to play_quiz_path
@@ -22,6 +27,7 @@ class QuizzesController < ApplicationController
   end
 
   def show
+    authorize @quiz
     @questions = QuestionQuiz.where(quiz_id: @quiz.id)
   rescue StandardError => e
     render body: e.message
@@ -30,6 +36,7 @@ class QuizzesController < ApplicationController
   def create
     @quiz = Quiz.new(quiz_params)
     @quiz.user_id = current_user.id
+    authorize @quiz
     @quiz.user_answer = @quiz.user_answer.filter { |a| !a.empty? }
     @quiz.question_ids = quiz_params[:question_ids][0].split
     @quiz.category_ids = quiz_params[:category_ids][0].split
@@ -49,6 +56,7 @@ class QuizzesController < ApplicationController
   end
 
   def destroy
+    authorize @quiz
     @quiz.destroy
     redirect_to quizzes_path, status: :see_other
   rescue StandardError => e
