7fb9c97a88c01791e22ddddb640e5ed6dfa99fba
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-05-06 00:50:49 +0545

    feat:add initial mailer file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,15 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    user_name: 'sagarvandari1@gmail.com',
+    password: 'jdtvrynbhvircgax',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
---
+++ spec/mailers/previews/quiz_system_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/quiz_system
+class QuizSystemPreview < ActionMailer::Preview
+
+end
