286ededb65718bc4b9c174ab004a9b7c16b9030d
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-05-10 11:43:14 +0545

    style:rendering pdf in better way

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,2 +1,6 @@
 module QuizzesHelper
+  def quiz_categories(quiz)
+    name = quiz.categories.map(&:name)
+    name.join(',')
+  end
 end
--- app/mailers/quiz_system_mailer.rb
+++ app/mailers/quiz_system_mailer.rb
@@ -2,7 +2,6 @@ class QuizSystemMailer < ApplicationMailer
   default from: 'sagarvandari@gmail.com'
   def welcome_email
     @user = params[:user]
-    @url = 'https://example.com/login'
     @quiz = params[:quiz]
     @questions = params[:question]
     pdf1 = QuizzesController.new.render_to_string(
@@ -12,7 +11,7 @@ class QuizSystemMailer < ApplicationMailer
                 :@questions => @questions }
     )
     pdf = Grover.new(pdf1).to_pdf
-    attachments['image.pdf'] = pdf
+    attachments['QUIZ.pdf'] = pdf
     mail(to: @user.email, subject: 'Welcome to Quiz System')
   end
 end
