74cca6c760b4184a9472c287e03a7966c4c1ab87
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-05-12 12:40:17 +0545

    feat:refactor quiz model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_060815) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_12_061912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,7 +80,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_060815) do
     t.text "report"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
+    t.string "email"
     t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
