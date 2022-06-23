9044b3bbdb3650f948b38c8593a32c200aed5b18
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-06-23 10:01:16 +0545

    fix:write test cases for quiz models

--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -3,7 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe('Quizzes', type: :request) do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+  before { sign_in(user) }
+  describe 'when request is performed with role admin' do
+    let(:user) { create :user, role: 'admin' }
+    context 'GET /index' do
+      it 'can get the quizzes' do
+        get quizzes_path
+        response.should(have_http_status(200))
+      end
+      it 'can render index page' do
+        get quizzes_path
+        response.should(render_template('index'))
+      end
+    end
+    context 'GET/new' do
+      it 'should raise error if no category is selected' do
+        get new_quiz_path, params: { category_ids: [] }
+        expect(flash[:error]).to(eq('Please Select at least One Category'))
+      end
+
+      it 'should fetch at most 8 questions' do
+        category1 = create(:category)
+        category2 = create(:category)
+        get new_quiz_path, params: { category_ids: [' ', category1.id, category2.id] }
+        Quiz.first.questions.count.should(eq(8))
+      end
+    end
   end
 end
